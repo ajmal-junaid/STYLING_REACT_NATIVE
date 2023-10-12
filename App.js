136caaeef00338ac482eeb4ef3c0151665d0d1b9
Ajmal junaid <ajmal6201@gmail.com>
2023-10-12 21:21:28 +0530

    basic styling

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: "plum", padding:60 }}>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <View style={styles.lightBlueBox}>
+        <Text styles={styles.title}>Light Blue box</Text>
+      </View>
+      <View style={styles.lightGreenBox}>
+        <Text styles={styles.title}>Light Green box</Text>
+      </View>
     </View>
-  )
+  );
 }
+
+export const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: "plum", padding: 60 },
+  lightBlueBox: { 
+    backgroundColor: "lightblue",
+    width: 100,
+    height: 100,
+    padding: 10
+  },
+  lightGreenBox: { 
+    backgroundColor: "lightgreen",
+    width: 100,
+    height: 100,
+    padding: 10
+  },
+});
